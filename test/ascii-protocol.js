355ea6c68ebaa4d1bf7e38dd1cec5563a022871b
3rd-Eden <info@3rd-Eden.com>
2012-06-23 01:10:12 +0200

    Working on the suite

--- test/ascii-protocol.js
+++ test/ascii-protocol.js
@@ -4,7 +4,24 @@ describe('ascii-protocol', function () {
 
   var Protocol = require('../lib/protocol/ascii');
 
+  it('should exported as a function', function () {
+    expect(Protocol).to.be.a('function');
+  });
+
   it('should be an instance of Stream', function () {
-    expect(new Protocol).to.be.an.instanceof(require('stream'));
+    expect(new Protocol()).to.be.an.instanceof(require('stream'));
+  });
+
+  it('should automatically convert strings to ints', function () {
+    var parser = new Protocol('100');
+
+    expect(parser.limit).to.be.a('number');
+    expect(parser.limit).to.eql(100);
+  });
+
+  it('should be a writable stream', function () {
+    var parser = new Protocol();
+
+    expect(parser.writable).to.eql(true);
   });
 });
